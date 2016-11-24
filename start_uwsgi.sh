7358a765383fe3001f6808198a38502819580201
jv <vasyuk.eugene@gmail.com>
2016-11-23 18:18:29 -0800

    start script info

--- start_uwsgi.sh
+++ start_uwsgi.sh
@@ -1,24 +1,32 @@
+# prepare log output folder
 sudo mkdir /tmp/uwsgi
 
-source chat_env/bin/activate
+# prepare virtualenv
+sudo mkdir v_env
+source v_env/bin/activate
 
+# prepare project requirements tools
 sudo apt-get install python-pip
 #pip install --upgrade pip
 sudo pip install uwsgi
 
+# prepare project requirements
 echo "updating requirements..."
 pip install -r requirements.txt
 
+# prepare mongo service
 #echo "starting mongo"
 #exec service mongodb start &
 #echo "mongo should be started already"
 
+# hot-fix of import problem
 echo "EXPORT PYTHONPATH"
 export PYTHONPATH=${PYTHONPATH}:/code/pychatty/
 
+# start service itself
 echo "starting uwsgi"
 exec uwsgi --ini uwsgi.ini --pidfile /tmp/pychatty.pid &
 #exec uwsgi --socket 0.0.0.0:8181 --protocol=http -w pychatty &
 
-
+# exit virtualenv
 deactivate
