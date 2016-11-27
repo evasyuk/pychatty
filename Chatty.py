2c63c5fbc02349ad27eac1847be3765fa9bc5505
jv <vasyuk.eugene@gmail.com>
2016-11-27 02:26:47 -0800

    patch for form-url-encode POST parameters

--- Chatty.py
+++ Chatty.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request
 from core import API as api
 
@@ -21,9 +22,13 @@ def hello_world():
 @application.route('/login', methods=['POST'])
 def route_login():
     # params
-    params = request.get_json()
+    params = params_from_request(request)
 
-    return api.login(params=params)
+    if params is None:
+        response = json.dumps({'error': "unsupported content-type -> " + str(request.content_type)})
+        return response, 400
+    else:
+        return api.login(params=params)
 
 ######
 # updates
@@ -37,11 +42,15 @@ def route_update():
         return api.on_update_request(headers=headers)
     elif request.method == 'POST':
         # headers
-        # params
-        params = request.get_json()
         headers = request.headers
+        # params
+        params = params_from_request(request)
 
-        return api.on_new_msg(params=params, headers=headers)
+        if params is None:
+            response = json.dumps({'error': "unsupported content-type -> " + str(request.content_type)})
+            return response, 400
+        else:
+            return api.on_new_msg(params=params, headers=headers)
 
 ######
 # users
@@ -49,9 +58,13 @@ def route_update():
 @application.route('/user/add', methods=['POST'])
 def route_user_add():
     # params
-    params = request.get_json()
+    params = params_from_request(request)
 
-    return api.on_add_user(params=params)
+    if params is None:
+        response = json.dumps({'error': "unsupported content-type -> " + str(request.content_type)})
+        return response, 400
+    else:
+        return api.on_add_user(params=params)
 
 
 @application.route('/user/me', methods=['GET'])
@@ -82,6 +95,28 @@ def route_user_add_friend(friend_uid):
         headers = request.headers
 
         return api.on_remove_friend(headers=headers, friend_uid=friend_uid)
+
+
+def params_from_request(request_this):
+    """
+    This method transforms parameters from different types or POST requests:
+    - either application/json
+    - either application/form-url-encoded
+
+    :param request_this:
+    :return:
+    """
+    if str(request_this.content_type) == 'application/x-www-form-urlencoded':
+        params = dict()
+
+        for key, value in zip(request_this.form.keys(), request_this.form.values()):
+            params[key] = value
+    elif str(request_this.content_type) == 'application/json':
+        params = request_this.get_json()
+    else:
+        return None
+
+    return params
 
 
 if __name__ == '__main__' or __name__ == 'uwsgi_file_Chatty':
